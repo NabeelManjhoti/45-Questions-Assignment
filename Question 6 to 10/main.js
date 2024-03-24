9f47d5ec94ce98513f11ae2f47c04a54ab5dd899
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-03-24 22:18:28 +0500

    Question 6 to 10

---
+++ Question 6 to 10/main.js
@@ -0,0 +1,32 @@
+// Question 6
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// Answer
+let personName = "\t\n Bilal \t\n";
+console.log(personName);
+console.log(personName.trim());
+// Question 7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Answer
+console.log(5 + 3);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(16 / 2);
+// Question 8
+/* You should create four lines that look like this:
+console.log(5 + 3)*/
+// Answer
+console.log(5 + 3);
+// Question 9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+// Answer
+let favoriteNumber = 5;
+console.log(`My favorite number is ${favoriteNumber}`);
+export {};
+// Question 10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Answer
+// one line comment.
+/* Multi lines comments
+this will be one line,
+two lines
+or as much lines you wanted. */ 
