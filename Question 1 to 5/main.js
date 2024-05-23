d30fe556af87fed9e44907c061dc6f9f9a6f5bcd
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-05-23 23:42:32 +0500

    2nd Commit

--- Question 1 to 5/main.js
+++ Question 1 to 5/main.js
@@ -1,27 +1,21 @@
-// Question 1
-// Install Node.js, TypeScript and VS Code on your computer.
-// Answer
-// Already installed
-// Question 2
-// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-// Answer
-let personName = "Aisha";
-console.log("Hello personName, would you like to learn some python today.");
-// Question 3
-// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-// Answer
-let personName2 = "Bilal";
-console.log(personName2.toLowerCase());
-console.log(personName2.toUpperCase());
-console.log(personName2.charAt(0).toUpperCase() + personName2.slice(1).toLowerCase());
-// Question 4
-// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-// Answer
-console.log('Elbert Hubbard once said "When life gives you lemons, make lemonade."');
-// Question 5
-// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-// Answer
-let famous_person = "Benjamin Franklin";
-let message = `${famous_person} once said, "Time is money."`;
-console.log(message);
+// Question:1 Install Node.js, TypeScript and VS Code on your computer.
+// Answer: Already Installed.
+// Question:2 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?"
+// Answer:
+let personName = "Nabeel";
+console.log(`Hello ${personName}, would you like to learn some TypeScript Today.`);
+// Question:3 Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// Answer:
+let person = "Nabeel Ali";
+console.log("lowercase:", person.toLowerCase());
+console.log("UPPERCASE:", person.toUpperCase());
+console.log("TitleCase:", person.charAt(0).toUpperCase() + person.slice(1).toLowerCase());
+// Question:4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+//Answer:
+console.log('Mola Ali as once said, "I will be patient, until even patience tires of my patience."');
+// Question:5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// Answer:
+let famousPerson = "Mola Ali as";
+console.log(`${famousPerson} once said, "I will be patient, until even patience tires of my patience."`);
 export {};
--- Question 1 to 5/main.ts
+++ Question 1 to 5/main.ts
@@ -1,38 +1,25 @@
-// Question 1
-// Install Node.js, TypeScript and VS Code on your computer.
+// Question:1 Install Node.js, TypeScript and VS Code on your computer.
+  // Answer: Already Installed.
 
-// Answer
-// Already installed
+// Question:2 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?"
+  // Answer:
+    let personName:string = "Nabeel";
+    console.log(`Hello ${personName}, would you like to learn some TypeScript Today.`);
 
-// Question 2
-// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// Question:3 Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+  // Answer:
+    let person:string = "Nabeel Ali";
+    console.log("lowercase:", person.toLowerCase());
+    console.log("UPPERCASE:", person.toUpperCase());
+    console.log("TitleCase:", person.charAt(0).toUpperCase() + person.slice(1).toLowerCase());
 
-// Answer
-let personName:string = "Aisha";
+// Question:4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 
-console.log("Hello personName, would you like to learn some python today.");
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+  //Answer:
+  console.log('Mola Ali as once said, "I will be patient, until even patience tires of my patience."');
 
-// Question 3
-// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-// Answer
-let personName2:string = "Bilal";
-
-console.log(personName2.toLowerCase());
-console.log(personName2.toUpperCase());
-console.log(personName2.charAt(0).toUpperCase() + personName2.slice(1).toLowerCase());
-
-// Question 4
-// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-
-// Answer
-console.log('Elbert Hubbard once said "When life gives you lemons, make lemonade."');
-
-// Question 5
-// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-
-// Answer
-let famous_person:string = "Benjamin Franklin";
-let message:string = `${famous_person} once said, "Time is money."`;
-
-console.log(message);
+// Question:5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// Answer:
+  let famousPerson:string = "Mola Ali as";
+  console.log(`${famousPerson} once said, "I will be patient, until even patience tires of my patience."`);
