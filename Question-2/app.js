473212632a79470a35a34f6ef11a76fc5d2e8721
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-15 23:52:44 +0500

    1st Commit

---
+++ Question-2/app.js
@@ -0,0 +1,4 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+//Answer.
+var personName = "Nabeel";
+console.log("Hi ".concat(personName, ", Would you like to drink some Tea"));
