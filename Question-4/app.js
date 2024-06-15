0b5e0ed5dc6bf28fe42beaf184a58bafbd097e0f
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 00:08:05 +0500

    1st Commit

---
+++ Question-4/app.js
@@ -0,0 +1,4 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+//Answer.
+console.log('Mola Ali as once said, "ear God and you will have no cause to fear any one."');
