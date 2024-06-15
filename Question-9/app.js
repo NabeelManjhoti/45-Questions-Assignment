6045e58f6b3f395ebe19919460ba157fd4ffb1dc
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 00:58:13 +0500

    1st Commit

---
+++ Question-9/app.js
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+//Answer.
+var favoriteNumber = 5;
+var message = "My favorite Number is ".concat(favoriteNumber);
+console.log(message);
---
+++ Question-9/app.ts
@@ -0,0 +1,6 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+//Answer.
+  let favoriteNumber:number = 5;
+  let message:string = `My favorite Number is ${favoriteNumber}`;
+  console.log(message);
