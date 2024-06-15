d42b3818ae43aa4229749c22627665e1d5584606
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 00:42:35 +0500

    2nd Commit

--- Question-7/app.ts
+++ Question-7/app.ts
@@ -1,13 +1,6 @@
 //Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
 
-let addition:number = 5 + 3;
-console.log(`Addition ${addition}`);
-
-let subtraction:number = 10 - 2;
-console.log(`Subtraction ${subtraction}`);
-
-let multiplication:number = 4 * 2;
-console.log(`Multiplication ${multiplication}`);
-
-let devision:number = 24 / 3;
-console.log(`Devision ${devision}`);
+console.log(5 + 3); //Addition
+console.log(10 - 2); //Substraction
+console.log(4 * 2); //Multiplication
+console.log(16 / 2); //Devision
