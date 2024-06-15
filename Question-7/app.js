7ac94235fb8a00edb3da1c361989a05f5e8d4cb0
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 00:35:14 +0500

    1st Commit

---
+++ Question-7/app.js
@@ -0,0 +1,9 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+var addition = 5 + 3;
+console.log("Addition ".concat(addition));
+var subtraction = 10 - 2;
+console.log("Subtraction ".concat(subtraction));
+var multiplication = 4 * 2;
+console.log("Multiplication ".concat(multiplication));
+var devision = 24 / 3;
+console.log("Devision ".concat(devision));
---
+++ Question-7/app.ts
@@ -0,0 +1,13 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let addition:number = 5 + 3;
+console.log(`Addition ${addition}`);
+
+let subtraction:number = 10 - 2;
+console.log(`Subtraction ${subtraction}`);
+
+let multiplication:number = 4 * 2;
+console.log(`Multiplication ${multiplication}`);
+
+let devision:number = 24 / 3;
+console.log(`Devision ${devision}`);
