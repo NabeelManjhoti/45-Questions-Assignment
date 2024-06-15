3f2fe85325b8061d60a419e2c559a754a340c00a
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 00:14:40 +0500

    1st Commit

---
+++ Question-5/app.js
@@ -0,0 +1,4 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+//Answer.
+var famousPerson = "Mola Ali AS";
+console.log("".concat(famousPerson, " once said, \"Fear God and you will have no cause to fear any one.\""));
