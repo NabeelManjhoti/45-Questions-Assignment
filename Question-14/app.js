e30c33a90085190cd972b87529b7a4d109ed5cb7
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 11:32:08 +0500

    1st Commit

---
+++ Question-14/app.js
@@ -0,0 +1,10 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+//Answer.
+var guestList = [
+    "Haseeb",
+    "Haris",
+    "Anas",
+];
+console.log("".concat(guestList[0], ", you are invited in today's Dinner."));
+console.log("".concat(guestList[1], ", you are invited in today's Dinner."));
+console.log("".concat(guestList[2], ", you are invited in today's Dinner."));
