81f551638ab59493494b4da028ba26af8c07858b
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 19:55:08 +0500

    1st Commit

---
+++ Question-19/main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//Answer.
+let guestList = [
+    "Sangeen",
+    "Saad",
+    "Suleman",
+    "Saad G",
+];
+console.log(`\n${guestList.length} guests are invited in today's Dinner.`);
+console.log(guestList.join(", "));
---
+++ Question-19/main.ts
@@ -0,0 +1,11 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+//Answer.
+  let guestList:string[] = [
+    "Sangeen",
+    "Saad",
+    "Suleman",
+    "Saad G",
+  ];
+  console.log(`\n${guestList.length} guests are invited in today's Dinner.`)
+  console.log(guestList.join(", "));
