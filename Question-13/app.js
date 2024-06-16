0777b4756e174e59bbba5e3e832bf10fc73d5ecc
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 11:26:27 +0500

    1st Commit

---
+++ Question-13/app.js
@@ -0,0 +1,9 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+//Answer.
+var dreamCars = [
+    "BMW",
+    "Tesla",
+    "Audi",
+    "Ferarri",
+];
+console.log("I would like to own ".concat(dreamCars[2]));
---
+++ Question-13/app.ts
@@ -0,0 +1,11 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+//Answer.
+  let dreamCars:string[] = [
+    "BMW",
+    "Tesla",
+    "Audi",
+    "Ferarri",
+  ];
+
+  console.log(`I would like to own ${dreamCars[2]}`);
