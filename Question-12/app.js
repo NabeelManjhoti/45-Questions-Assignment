7faa894104ee553f04cd9dc8a0c2102ba6ab0417
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-16 11:19:14 +0500

    1st Commit

---
+++ Question-12/app.js
@@ -0,0 +1,12 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+//Answer.
+var names = [
+    "Haseeb",
+    "Haris",
+    "Anas",
+    "Kareem",
+];
+console.log("Hi ".concat(names[0], ", Would you like to learn some TypeScript today"));
+console.log("Hi ".concat(names[1], ", Would you like to learn some TypeScript today"));
+console.log("Hi ".concat(names[2], ", Would you like to learn some TypeScript today"));
+console.log("Hi ".concat(names[3], ", Would you like to learn some TypeScript today"));
---
+++ Question-12/app.ts
@@ -0,0 +1,13 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+//Answer.
+  let names:string[] = [
+    "Haseeb",
+    "Haris",
+    "Anas",
+    "Kareem",
+  ];
+  console.log(`Hi ${names[0]}, Would you like to learn some TypeScript today`);
+  console.log(`Hi ${names[1]}, Would you like to learn some TypeScript today`);
+  console.log(`Hi ${names[2]}, Would you like to learn some TypeScript today`);
+  console.log(`Hi ${names[3]}, Would you like to learn some TypeScript today`);
