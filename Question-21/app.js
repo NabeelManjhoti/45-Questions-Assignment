154a1e29b94ef23c04f32f00c1ee2c6133481faf
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-17 17:24:02 +0500

    1st

---
+++ Question-21/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+//Answer.
+let realMadrid = {
+    num1: "Courtous",
+    num2: "Carvajal",
+    num3: "Militao",
+    num4: "Alaba",
+    num5: "Bellingham",
+};
---
+++ Question-21/app.ts
@@ -0,0 +1,16 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+//Answer.
+  let realMadrid: {
+    num1:string,
+    num2:string,
+    num3:string,
+    num4:string,
+    num5:string,
+  } = {
+    num1: "Courtous",
+    num2: "Carvajal",
+    num3: "Militao",
+    num4: "Alaba",
+    num5: "Bellingham",
+  };
