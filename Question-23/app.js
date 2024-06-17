984be2eed5d18d2cee95e4c3b9da867e4d09ab09
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-06-17 18:14:05 +0500

    1st Commit

---
+++ Question-23/app.js
@@ -0,0 +1,33 @@
+"use strict";
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+//Answer.
+let isIphone = "Mobile";
+console.log(isIphone === "Mobile" ? true : false);
+let isBabarAzam = "Cricketer";
+console.log(isBabarAzam === "Cricketer" ? true : false);
+let isUndertaker = "Wrestler";
+console.log(isUndertaker === "Wrestler" ? true : false);
+let isTypeScript = "Programming Language";
+console.log(isTypeScript === "Programming Language" ? true : false);
+let isNextJs = "Frontend";
+console.log(isNextJs === "Frontend" ? true : false);
+let isUpwork = "Social Media";
+console.log(isUpwork === "Freelance Market" ? true : false);
+let isLinkedin = "Blogging Site";
+console.log(isLinkedin === "Resume Site" ? true : false);
+let isHp = "Smartphone Brand";
+console.log(isHp === "Laptop Brand" ? true : false);
+let isSamsung = "Schooling System";
+console.log(isSamsung === "Electronic Brand" ? true : false);
+let isLion = "Bird";
+console.log(isLion === "Animal" ? true : false);
---
+++ Question-23/app.ts
@@ -0,0 +1,42 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+//Answer.
+  let isIphone:string = "Mobile";
+  console.log(isIphone === "Mobile" ?true :false);
+
+  let isBabarAzam:string = "Cricketer";
+  console.log(isBabarAzam === "Cricketer" ?true :false);
+
+  let isUndertaker:string = "Wrestler";
+  console.log(isUndertaker === "Wrestler" ?true :false);
+
+  let isTypeScript:string = "Programming Language";
+  console.log(isTypeScript === "Programming Language" ?true :false);
+
+  let isNextJs:string = "Frontend";
+  console.log(isNextJs === "Frontend" ?true :false);
+
+  let isUpwork:string = "Social Media";
+  console.log(isUpwork === "Freelance Market" ?true :false);
+
+  let isLinkedin:string = "Blogging Site";
+  console.log(isLinkedin === "Resume Site" ?true :false);
+
+  let isHp:string = "Smartphone Brand";
+  console.log(isHp === "Laptop Brand" ?true :false);
+
+  let isSamsung:string = "Schooling System";
+  console.log(isSamsung === "Electronic Brand" ?true :false);
+
+  let isLion:string = "Bird";
+  console.log(isLion === "Animal" ?true :false);
